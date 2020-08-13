75b53af724e26edbe5c74c4e1a7f09b94ca7750e
YoungChulShin <go1323@gmail.com>
2020-08-14 04:18:39 +0900

    페이징 추가

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -26,8 +30,17 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // SpringBoot에서 Pageable이 있으면 PageRequest로 변경해서 채워준다
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable).map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i));
+        }
     }
 }
