194a91054b712ccb04ba07f63c08c87098363202
YoungChulShin <go1323@gmail.com>
2020-10-20 21:29:10 +0900

    return type

--- 2nd/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 2nd/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -143,4 +143,13 @@ class MemberRepositoryTest {
             System.out.println(s);
         }
     }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+    }
 }
